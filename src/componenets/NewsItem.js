9c4ae9f98b31dd6679a693418fab6baec18600d8
harshilAmbliya <harshilambliya15@gmail.com>
2023-01-14 13:56:19 +0530

    feat:newsapi call

---
+++ src/componenets/NewsItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+   
+    render() {
+        let { title, description, imgurl, newsurl } = this.props;
+        return (
+            <>
+                
+                    <div className=" my-3">
+                        <div className="card h-100">
+                            <img src={imgurl} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{title}</h5>
+                                <p className="card-text">{description}</p>
+                                <a href={newsurl} target="_blank" className='btn btn-sm btn-primary'>Read More</a>
+                            </div>
+                        </div>
+                    </div>  
+                
+            </>
+        )
+    }
+}
+
+export default NewsItem
