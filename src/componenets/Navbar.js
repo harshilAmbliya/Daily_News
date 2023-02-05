b4903a3beddcc3d3e1247ce4720b08e36861364f
harshilAmbliya <harshilambliya15@gmail.com>
2023-02-05 21:22:01 +0530

    fix: api data fetch issue

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/">Daily - News</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +14,7 @@ export default class Navbar extends Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                   <Link className={`nav-link  `}aria-current="page" to="/">General</Link>
+                                   <Link className={`nav-link`}aria-current="page" to="/">General</Link>
                                 </li>
                                 <li className="nav-item">
                                    <Link className="nav-link" to="/sports">Sports</Link>
